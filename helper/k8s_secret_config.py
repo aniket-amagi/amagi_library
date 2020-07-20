ebb440e6036b58c4a7e14cd7c1ae23046f577eb1
Ubuntu <ubuntu@ip-172-31-44-130.ec2.internal>
2020-07-20 15:17:13 +0000

    Support for Argo - AWS details via k8s secrets

---
+++ helper/k8s_secret_config.py
@@ -0,0 +1,42 @@
+import json
+import logging
+import os
+import traceback
+
+from amagi_library.boto3_helper.client import Client
+from amagi_library.boto3_helper.s3 import DisplayS3Object
+
+class K8SSecretConfig(object):
+    """
+        Retrieves configuration from S3 using AWS creds from K8S secrets available as env variable
+    """
+    @staticmethod
+    def aws_api_response_handler(response):
+        """
+        This method recursively add data into object Dictionary
+        :param response: list_objects_response from boto3 s3 client
+        """
+
+        if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+            logging.info("Sent successfully")
+        else:
+            logging.error("Response from get : " + str(response["ResponseMetadata"]))
+
+    def read_from_s3(self, **kwargs):
+        """
+        Driving method which will get contents of all the objects in s3
+        :return: Returns object dict containing details about s3
+        """
+        self.s3_details = kwargs["s3_details"]
+        aws_details_str = os.environ['AWS_DETAILS'] if "AWS_DETAILS" in os.environ else None
+        if aws_details_str is None:
+            return None
+        try:
+            aws_details_obj = json.loads(aws_details_str)
+            obj = DisplayS3Object(aws_details=aws_details_obj)
+            data = obj.object_content(s3_details=kwargs['s3_details'],object_path=kwargs['s3_details']['object_path'])
+            if data is not None:
+                return json.loads(data)
+        except BaseException:
+            logging.error("Uncaught exception in secret_config.py " + traceback.format_exc())
+            raise BaseException("Problem in secret_config.py" + obj.aws_details)
